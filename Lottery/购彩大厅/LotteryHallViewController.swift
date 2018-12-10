5b9e3483436b2d3213c23713dcbebff5dec4815f
薛永伟 <xueyongwei@foxmail.com>
2018-12-10 18:46:42 +0800

    demo

---
+++ Lottery/购彩大厅/LotteryHallViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LotteryHallViewController.swift
+//  Lottery
+//
+//  Created by 薛永伟 on 2018/12/10.
+//  Copyright © 2018年 薛永伟. All rights reserved.
+//
+
+import UIKit
+
+class LotteryHallViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
