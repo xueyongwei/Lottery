3d1558004f300cf39bad4bc1f7dec683909cc209
薛永伟 <xueyongwei@foxmail.com>
2018-12-10 13:55:58 +0800

    Initial Commit

---
+++ Lottery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lottery
+//
+//  Created by 薛永伟 on 2018/12/10.
+//  Copyright © 2018年 薛永伟. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
