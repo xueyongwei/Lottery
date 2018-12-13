bc957798ee5854a12309b0a119c0dbe4b809865e
薛永伟 <xueyongwei@foxmail.com>
2018-12-13 19:07:50 +0800

    首页信息流

---
+++ Lottery/Common/BaseClass/CodeLayoutReusableView.swift
@@ -0,0 +1,25 @@
+//
+//  CodeLayoutReusableView.swift
+//  Lottery
+//
+//  Created by 薛永伟 on 2018/12/13.
+//  Copyright © 2018年 薛永伟. All rights reserved.
+//
+
+import UIKit
+
+class CodeLayoutReusableView: UICollectionReusableView ,ViewCodeLayoutable{
+    func codeCustomSubviews() {
+        XYWDebugLog("CodeLayoutView派生类应该遵循ViewCodeLayoutable协议", type: .error)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        codeCustomSubviews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        codeCustomSubviews()
+    }
+}
